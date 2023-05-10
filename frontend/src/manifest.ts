9bc5ef8964ee941933cd30b5b83ad3c35cc477d1
shgus1224 <shgus1224@naver.com>
2023-05-11 01:56:17 +0900

    fix: 서비스명 수정

--- frontend/src/manifest.ts
+++ frontend/src/manifest.ts
@@ -1,7 +1,7 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
 export default defineManifest({
-  name: 'cddd',
+  name: '유튜브 인터랙티브 자동자막 생성 서비스',
   description: '',
   version: '0.0.0',
   manifest_version: 3,
