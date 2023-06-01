b5f7399aeb28aa973f66d0e0d190a4098cec0670
shgus1224 <shgus1224@naver.com>
2023-06-01 23:07:40 +0900

    fix: 돔 제어하는 중복코드 함수화

--- frontend/src/content/index.js
+++ frontend/src/content/index.js
@@ -8,6 +8,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // 감정분석 전 후로 subtitle 변경
   subtitle = message.data.textObj
   if (message.message == 'gotSubtitle') {
+    openScript()
     for (const i in subtitle) {
       timeArray.push(i)
     }
@@ -22,119 +23,10 @@ const main = () => {
     const target = document.querySelector('#body.ytd-transcript-search-panel-renderer')
     const testTarget = document.getElementsByClassName('ytp-time-display notranslate')[0]
 
-    // 2. 옵저버 콜백 생성
-    const callback = (mutationList, observer) => {
-      const target = document.querySelector('#body.ytd-transcript-search-panel-renderer')
-      const textClass = target.getElementsByClassName(
-        'style-scope ytd-transcript-segment-list-renderer active',
-      )
-
-      const text = textClass[0].innerText.split('\n')[1]
-      const time = textClass[0].innerText.split('\n')[0]
-
-      let buttonSelector = belowId.querySelector('button')
-      currentTag.innerText = text
-    }
-
-    const testCallback = () => {
-      const target = document.querySelector('#body.ytd-transcript-search-panel-renderer')
-      const textClass = target.getElementsByClassName(
-        'style-scope ytd-transcript-segment-list-renderer active',
-      )
-      // innerText 존재하지 않는 경우 분기처리
-      if (textClass[0] == undefined) {
-        return
-      }
-      const currentTime = textClass[0].innerText.split('\n')[0]
-
-      let buttonSelector = belowId.querySelector('button')
-
-      if (subtitle[currentTime] !== undefined) {
-        currentTag.innerText = subtitle[currentTime][0]
-        const prevTimeIndex = timeArray.indexOf(currentTime) - 1
-        const nextTimeIndex = timeArray.indexOf(currentTime) + 1
-        if (prevTimeIndex < 0 || timeArray[prevTimeIndex] == '0:00') {
-          prevTextTag.innerText = ''
-        } else {
-          prevTextTag.innerText = subtitle[timeArray[prevTimeIndex]][0]
-        }
-
-        if (nextTimeIndex > timeArray.length - 1) {
-          nextTextTag.innerText = ''
-        } else {
-          nextTextTag.innerText = subtitle[timeArray[nextTimeIndex]][0]
-        }
-
-        if (subtitle[currentTime][1] !== undefined) {
-          const emotionValue = subtitle[currentTime][1]
-          if (emotionValue > 70) {
-            currentTag.style.fontSize = '20px'
-            currentTag.style.color = 'black'
-            console.log('긍정', emotionValue)
-            currentTag.style.color = 'blue'
-          } else if (emotionValue < 30) {
-            currentTag.style.fontSize = '20px'
-            currentTag.style.color = 'black'
-            console.log('부정', emotionValue)
-            currentTag.style.color = 'red'
-          } else {
-            console.log('중립', emotionValue)
-            currentTag.style.fontSize = '20px'
-            currentTag.style.color = 'black'
-          }
-        }
-      } else {
-        let prevTime
-        for (const key in subtitle) {
-          // 비디오 시간과 자막시간을 비교하기 위함
-          let subitlteTime = Number(key.split(':').join(''))
-          let videoTime = Number(currentTime.split(':').join(''))
-          if (subitlteTime > videoTime) {
-            currentTag.innerText = subtitle[prevTime][0]
-            const emotionValue = subtitle[prevTime][1]
-            if (emotionValue > 70) {
-              currentTag.style.fontSize = '20px'
-              currentTag.style.color = 'black'
-              console.log('긍정', emotionValue)
-              currentTag.style.color = 'blue'
-            } else if (emotionValue < 30) {
-              currentTag.style.fontSize = '20px'
-              currentTag.style.color = 'black'
-              console.log('부정', emotionValue)
-              currentTag.style.color = 'red'
-            } else {
-              console.log('중립', emotionValue)
-              currentTag.style.fontSize = '20px'
-              currentTag.style.color = 'black'
-            }
-          } else {
-            prevTime = key
-          }
-        }
-      }
-    }
-
-    // 3. 옵저버 인스턴스 생성
-    const observer = new MutationObserver(callback) // 타겟에 변화가 일어나면 콜백함수를 실행하게 된다.
-    const testObserver = new MutationObserver(testCallback)
-
-    // 4. DOM의 어떤 부분을 감시할지를 옵션 설정
-    const config = {
-      childList: true, // 타겟의 하위 요소 추가 및 제거 감지
-      attributes: true, // 타켓의 속성 변경를 감지
-      characterData: true, // 타겟의 데이터 변경 감지
-      subtree: true, // 타겟의 자식 노드 아래로도 모두 감지
-      attributeOldValue: false, // 타겟의 속성 변경 전 속성 기록
-      characterDataOldValue: false, // 타겟의 데이터 변경 전 데이터 기록
-    }
-
-    // 5. 감지 시작
-    if (target != null) {
-      clearInterval(startMutation)
-      alert('자막 생성을 시작합니다.')
+    // 자막박스 추가
+    const handleSubtitleBoxMake = () => {
       var belowId = document.getElementById('below')
 
-      // 자막박스 추가
       var subtitleBoxTag = document.createElement('button')
       subtitleBoxTag.style.width = '100%'
       subtitleBoxTag.style.minHeight = '50px'
@@ -161,9 +53,98 @@ const main = () => {
       subtitleBoxTagSelector.prepend(nextTextTag)
       subtitleBoxTagSelector.prepend(currentTag)
       subtitleBoxTagSelector.prepend(prevTextTag)
+    }
 
-      // observer.observe(target, config)
-      testObserver.observe(target, config)
+    // 감성수치에 따른 스타일 변경
+    const changeEmotionalStyle = (value) => {
+      if (value > 70) {
+        currentTag.style.fontSize = '20px'
+        currentTag.style.color = 'black'
+        console.log('긍정', value)
+        currentTag.style.color = 'blue'
+      } else if (value < 30) {
+        currentTag.style.fontSize = '20px'
+        currentTag.style.color = 'black'
+        console.log('부정', value)
+        currentTag.style.color = 'red'
+      } else {
+        console.log('중립', value)
+        currentTag.style.fontSize = '20px'
+        currentTag.style.color = 'black'
+      }
+    }
+
+    const callback = () => {
+      const target = document.querySelector('#body.ytd-transcript-search-panel-renderer')
+      const textClass = target.getElementsByClassName(
+        'style-scope ytd-transcript-segment-list-renderer active',
+      )
+      // innerText 존재하지 않는 경우 분기처리
+      if (textClass[0] == undefined) {
+        return
+      }
+      const currentTime = textClass[0].innerText.split('\n')[0]
+
+      if (subtitle[currentTime] !== undefined) {
+        currentTag.innerText = subtitle[currentTime][0]
+        const prevTimeIndex = timeArray.indexOf(currentTime) - 1
+        const nextTimeIndex = timeArray.indexOf(currentTime) + 1
+        if (prevTimeIndex < 0 || timeArray[prevTimeIndex] == '0:00') {
+          prevTextTag.innerText = ''
+        } else {
+          prevTextTag.innerText = subtitle[timeArray[prevTimeIndex]][0]
+        }
+
+        if (nextTimeIndex > timeArray.length - 1) {
+          nextTextTag.innerText = ''
+        } else {
+          nextTextTag.innerText = subtitle[timeArray[nextTimeIndex]][0]
+        }
+
+        if (subtitle[currentTime][1] !== undefined) {
+          const emotionValue = subtitle[currentTime][1]
+          changeEmotionalStyle(emotionValue)
+        }
+      } else {
+        let prevTime
+        for (const key in subtitle) {
+          // 비디오 시간과 자막시간을 비교하기 위함
+          let subitlteTime = Number(key.split(':').join(''))
+          let videoTime = Number(currentTime.split(':').join(''))
+          if (subitlteTime > videoTime) {
+            currentTag.innerText = subtitle[prevTime][0]
+            const emotionValue = subtitle[prevTime][1]
+            changeEmotionalStyle(emotionValue)
+          } else {
+            prevTime = key
+          }
+        }
+      }
+    }
+
+    // 3. 옵저버 인스턴스 생성
+    const observer = new MutationObserver(callback) // 타겟에 변화가 일어나면 콜백함수를 실행하게 된다.
+
+    // 4. DOM의 어떤 부분을 감시할지를 옵션 설정
+    const config = {
+      childList: true, // 타겟의 하위 요소 추가 및 제거 감지
+      attributes: true, // 타켓의 속성 변경를 감지
+      characterData: true, // 타겟의 데이터 변경 감지
+      subtree: true, // 타겟의 자식 노드 아래로도 모두 감지
+      attributeOldValue: false, // 타겟의 속성 변경 전 속성 기록
+      characterDataOldValue: false, // 타겟의 데이터 변경 전 데이터 기록
+    }
+
+    // 5. 감지 시작
+    if (target != null) {
+      clearInterval(startMutation)
+      alert('자막 생성을 시작합니다.')
+
+      handleSubtitleBoxMake()
+
+      observer.observe(target, config)
+    } else {
+      alert('자동자막을 제공하는 영상이 아닙니다.')
     }
   }
 
@@ -189,10 +170,10 @@ const openScript = async () => {
   main()
 }
 
-setTimeout(() => {
-  console.log('test')
-  openScript()
-}, 5000)
+// setTimeout(() => {
+//   console.log('test')
+//   openScript()
+// }, 5000)
 
 export default main
 printLine("Using the 'printLine' function from the Print Module")
